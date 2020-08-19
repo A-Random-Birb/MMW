9832317b9bc3935c8b78c96e1ebf5240684292ab
A-Random-Birb <36333036+A-Random-Birb@users.noreply.github.com>
2020-08-19 10:04:28 +0100

    added program

---
+++ start.bat
@@ -0,0 +1,23 @@
+@echo off
+IF EXIST "resources\url.txt" (
+  goto :url
+) ELSE (
+  goto :fts
+)
+
+:url
+set /p url=<resources\url.txt
+goto :start
+
+:start
+set /p msg="Enter msg: "
+curl -d "content=%msg%" %url%
+goto start
+
+:fts
+echo You have entered the first time setup. If you dont have a Discord Webhook url, then pleasde check README.txt for more informations
+set /p seturl="Please enter the webhook URL: "
+echo %seturl% > resources\url.txt
+echo Setup complete, starting main program
+pause
+goto :url
