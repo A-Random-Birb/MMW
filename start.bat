1e63769dc9820d3f9cdbf7e8ede92974b4974e6e
A-Random-Birb <36333036+A-Random-Birb@users.noreply.github.com>
2020-09-09 17:36:46 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,6 +15,7 @@ goto :start
 : : sends the contents of the msg variable to the url variable using curl, to whoever created curl, ily
 set /p msg="Enter msg: "
 curl -d "content=%msg%" %url%
+if ERRORLEVEL 1 goto :errorcurl
 goto start
 
 :fts
@@ -26,3 +27,6 @@ echo %seturl% > resources\url.txt
 echo Setup complete, starting main program
 pause
 goto :url
+
+:errorcurl
+echo oops, somethings gone wrong, read the error and try and fix it yourself. if you cant do that, open an issue on the Github page and I'll try to fix it
